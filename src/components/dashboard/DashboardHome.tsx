5aa5af9d335cfcf4ed386ae08e87510b81fc66ae
Shivayan Chakravarty <shivayanchak@gmail.com>
2026-01-30 19:03:52 +0530

    ui changes

--- src/components/dashboard/DashboardHome.tsx
+++ src/components/dashboard/DashboardHome.tsx
@@ -302,17 +302,17 @@ const DashboardHome = ({ syllabus, papers, student }: HomeProps) => {
 
       {/* Quick Actions */}
       <div ref={actionsRef} className="flex justify-between gap-4 mb-10 max-w-7xl mx-auto">
-        <div className="w-[50%] flex flex-col transition-all duration-300 relative overflow-hidden border border-purple-600/20 hover:bg-purple-50 text-black rounded-2xl p-3 sm:p-6">
+        <div className="w-[50%] flex flex-col transition-all duration-300 relative overflow-hidden border border-purple-600/20 dark:border-purple-500/70 dark:text-white hover:bg-purple-50/5 text-black rounded-2xl p-3 sm:p-6">
           <div className=" flex items-center gap-4">
             <div className="w-10 sm:w-14 h-10 sm:h-14 rounded-xl bg-purple-500 text-white backdrop-blur-sm flex items-center justify-center shrink-0">
               <Upload className="w-5 sm:w-7 h-5 sm:h-7" />
             </div>
             <div className="text-left">
               <h3 className="text-base sm:text-xl font-bold mb-1">Upload PYQ</h3>
-              <p className="text-black/70 text-sm">Add past year questions to expand the database</p>
+              <p className="text-black/70 dark:text-white/70 text-sm">Add past year questions to expand the database</p>
             </div>
           </div>
-          <button onClick={() => router.push('/dashboard/upload')} className="group w-1/2 ml-auto mt-5 cursor-pointer transition-all duration-300 hover:shadow-md hover:shadow-purple-500/50 bg-purple-500 px-10 rounded-md text-white h-10 flex items-center justify-center gap-2">
+          <button onClick={() => router.push('/dashboard/upload')} className="group w-1/2 ml-auto mt-5 cursor-pointer transition-all duration-300 hover:shadow-md hover:shadow-purple-500/50 bg-purple-500 font-semibold px-10 rounded-md text-white h-10 flex items-center justify-center gap-2">
             Upload PYQs
             <span className="inline-block transition-transform duration-200 group-hover:translate-x-1.5">
               →
@@ -320,17 +320,17 @@ const DashboardHome = ({ syllabus, papers, student }: HomeProps) => {
           </button>
         </div>
 
-        <div className="w-[50%] flex flex-col relative overflow-hidden transition-all duration-300 hover:bg-purple-50 border border-purple-600/20 text-black rounded-2xl p-3 sm:p-6">
+        <div className="w-[50%] flex flex-col relative overflow-hidden dark:text-white transition-all duration-300 hover:bg-purple-50/5 border border-purple-600/20 dark:border-purple-500/70 text-black rounded-2xl p-3 sm:p-6">
           <div className="relative z-10 flex items-center gap-4">
             <div className="w-10 sm:w-14 h-10 sm:h-14 rounded-xl bg-purple-500 text-white backdrop-blur-sm flex items-center justify-center shrink-0">
               <Play className="w-5 sm:w-7 h-5 sm:h-7" />
             </div>
             <div className="text-left">
               <h3 className="text-base sm:text-xl font-bold mb-1">Take Mock Test</h3>
-              <p className="text-black/80 text-sm">Practice with AI-generated questions</p>
+              <p className="text-black/80 dark:text-white/80 text-sm">Practice with AI-generated questions</p>
             </div>
           </div>
-          <button onClick={() => router.push('/dashboard/mock-test')} className="group w-1/2 ml-auto mt-5 cursor-pointer transition-all duration-300 hover:shadow-md hover:shadow-purple-500/50 bg-purple-500 px-10 rounded-md text-white h-10 flex items-center justify-center gap-2">
+          <button onClick={() => router.push('/dashboard/mock-test')} className="group w-1/2 ml-auto mt-5 font-semibold  cursor-pointer transition-all duration-300 hover:shadow-md hover:shadow-purple-500/50 bg-purple-500 px-10 rounded-md text-white h-10 flex items-center justify-center gap-2">
             Take mock tests
             <span className="inline-block transition-transform duration-200 group-hover:translate-x-1.5">
               →
