7d6a2981f082a523e97914816d806b83b57c3141
ParasharDeb <parashardeb11@gmail.com>
2026-01-31 00:42:00 +0530

    next-auth bug fixed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,11 +4,14 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-  GoogleProvider({
-    clientId: process.env.GOOGLE_CLIENT_ID||" ",
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET||" "
-  })
-]
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+    })
+  ]
 }
 
-export default NextAuth(authOptions)
+const handler = NextAuth(authOptions)
+
+// Change this line - export as named exports instead of default
+export { handler as GET, handler as POST }
